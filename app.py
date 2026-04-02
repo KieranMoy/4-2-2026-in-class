4aadceb2a6c6955f956abdde442fc30bc29565b4
KieranMoy <kiemoy@uw.edu>
2026-04-02 19:33:57 +0000

    Working Flask site

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    message = ""
+
+    if request.method == "POST":
+        message = request.form.get("message", "").strip()
+
+    return render_template("index.html", message=message)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=3000, debug=True)
